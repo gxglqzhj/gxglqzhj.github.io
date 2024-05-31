c68e423ffa1d243b6f758262ac8aae90f7faf7d5
gxglqzhj <gxglqzhj@163.com>
2024-05-31 21:58:18 +0800

    Update Hexo Static Content

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/12/hello-world/","2024/05/30/每日计划/"];function toRandomPost(){
+var posts=["2024/04/12/hello-world/","2024/05/30/每日计划/","2024/05/31/工程经济学/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
