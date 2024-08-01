93776deaeb807d7a7be8d6e2b86951e6c845d713
gxglqzhj <gxglqzhj@163.com>
2024-08-01 20:30:19 +0800

    Update Hexo Static Content

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["cn/NUdRGTH2/","cn/7xbit/","cn/xh01w/","cn/7xbi2/","cn/7x6c2/"];function toRandomPost(){
+var posts=["cn/NUdRGTH2/","cn/7xbit/","cn/xh01w/","cn/7xbi2/","cn/7x6c2/","cn/zxgjj/","cn/zxgjj/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
